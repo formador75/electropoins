168120004278129c8a79b3d6a08183bf56b650da
formador75 <misiontic.formador75@uis.edu.co>
2022-11-01 06:47:01 -0500

    agrego update ui

--- app/src/main/java/com/miempresa/electropoints/MainActivity.java
+++ app/src/main/java/com/miempresa/electropoints/MainActivity.java
@@ -6,23 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
+    private FirebaseAuth miAutenticacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogin = findViewById(R.id.button);
 
+        miAutenticacion = FirebaseAuth.getInstance();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,ActividadLogin.class);
+                miAutenticacion.signOut();
+                Intent i = new Intent(MainActivity.this, ActividadLogin.class);
                 startActivity(i);
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioActual = miAutenticacion.getCurrentUser();
+        actualizarUI(usuarioActual);
+    }
+
+    public void actualizarUI(FirebaseUser usuario){
+
+        if(usuario != null){
+            Toast.makeText(MainActivity.this, "Usted esta logeado", Toast.LENGTH_LONG).show();
+        }else{
+            Intent i = new Intent(MainActivity.this, ActividadLogin.class);
+            startActivity(i);
+        }
 
     }
 }
